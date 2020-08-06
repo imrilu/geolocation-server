778e2ca298caab781ce2e8c9e9e4098c1f67c23c
Imri <imri.luzzattov@gmail.com>
2020-08-06 14:09:21 +0300

    spring init

---
+++ src/main/java/com/geolocationserver/exercise/GeolocationServerApplication.java
@@ -0,0 +1,13 @@
+package com.geolocationserver.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeolocationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeolocationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geolocationserver/exercise/GeolocationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geolocationserver.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeolocationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
